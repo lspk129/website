01e14e8145315fdb01810c5a85d94e8f0a64795a
ALS <als@ALSs-MacBook-Pro.local>
2017-05-08 10:10:45 +0300

    Added image to LandingPage component

--- components/LandingPage/index.js
+++ components/LandingPage/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  background-image: url("http://trendytheme.net/demo2/wp/69/multipage/wp-content/uploads/revslider/web-product-dark/webproduct_darkbg2.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  height: 100vh
+`;
 
 export default () => (
-  <div>Landing Page</div>
+  <div>
+    <Wrapper />
+  </div>
 );
--- components/LandingPage/index.test.js
+++ components/LandingPage/index.test.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
+
 import LandingPage from './index';
 
-describe('LandingPage', () => {
-  it('renders without crashing', () => {
+describe('LandingPage component', () => {
+  it('renders styled-component Wrapper', () => {
+    const component = renderer.create(<LandingPage />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Wrapper component', () => {
+  it('renders style class with image', () => {
     const component = shallow(<LandingPage />);
     expect(component.find('div').length).toEqual(1);
   });
