093826fb4375005c19c2cf7d3fadd88502eef850
ALS <als@ALSs-MacBook-Pro.local>
2017-05-09 18:28:29 +0300

    Add hero title

---
+++ components/Hero/HeroTitle/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeroTitle = styled.h1`
+  text-align: center;
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 4rem;
+  white-space: nowrap;
+  font-weight: bold;
+  text-shadow: 0 0.1rem 0.1rem rgba(0, 0, 0, 0.4);
+  line-height: 4rem;
+`;
+
+export default ({ children }) => (
+  <HeroTitle>
+    { children }
+  </HeroTitle>
+);
+
+// moka <StyledColor>haiku</StyledColor>:<br />web app agency
--- components/Hero/HeroTitle/index.test.js
+++ components/Hero/HeroTitle/index.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import HeroText from './index';
+import HeroTitle from './index';
 
 describe('', () => {
   it('renders h1 element', () => {
-    const content = shallow(<HeroText />);
+    const content = shallow(<HeroTitle />);
 
     expect(content.find('h1').length).toEqual(1);
   });
 
   it('renders text "Moka Haiku: web app agency" ', () => {
-    const content = shallow(<HeroText />);
+    const content = shallow(
+      <HeroTitle>Moka Haiku:web app agency</HeroTitle>,
+    );
     expect(content.find('h1').text()).toEqual('Moka Haiku:web app agency');
   });
 });
