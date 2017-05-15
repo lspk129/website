5b072589e483ecf7ac5595f457239a58616c225d
ALS <l.spukas@gmail.com>
2017-05-15 20:19:23 +0300

    Add background image testing

--- components/Hero/HeroImage/index.js
+++ components/Hero/HeroImage/index.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const HomeImage = styled.div`
-  // background-image: url("http://trendytheme.net/demo2/wp/69/multipage/wp-content/uploads/revslider/web-product-dark/webproduct_darkbg2.jpg");
-  background: linear-gradient(210deg, #318EDF, #46c340);
+  background-image: url("http://trendytheme.net/demo2/wp/69/multipage/wp-content/uploads/revslider/web-product-dark/webproduct_darkbg2.jpg");
+  // background: linear-gradient(210deg, #318EDF, #46c340);
   background-position: center center;
   background-repeat: no-repeat;
   background-attachment: fixed;
