e9f5164550b75a4b6df9012eebb34038f14b8102
ALS <l.spukas@gmail.com>
2017-05-15 20:18:20 +0300

    Move body to separate component

--- pages/_document.js
+++ pages/_document.js
@@ -2,24 +2,26 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+import BodyContainer from '../components/Body/index';
+
 export default class MyDocument extends Document {
   render() {
     const sheet = new ServerStyleSheet();
     const main = sheet.collectStyles(<Main />);
     const styleTags = sheet.getStyleElement();
     return (
-      <html>
+      <html lang="en">
         <Head>
           <title>My page</title>
           {styleTags}
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
         </Head>
-        <body style={{ margin: 0 }}>
+        <BodyContainer>
           <div className="root">
             {main}
           </div>
           <NextScript />
-        </body>
+        </BodyContainer>
       </html>
     );
   }
