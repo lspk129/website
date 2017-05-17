ef0c59c3b03949cf799450e595c09702b56bfcc4
ALS <l.spukas@gmail.com>
2017-05-17 23:32:10 +0300

    Replace hero image

--- components/Hero/HeroImage/index.js
+++ components/Hero/HeroImage/index.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 
 const HomeImage = styled.div`
-  // background-image: url("http://trendytheme.net/demo2/wp/69/multipage/wp-content/uploads/revslider/web-product-dark/webproduct_darkbg2.jpg");
-  background: linear-gradient(210deg, #318EDF, #46c340);
-  background-position: center center;
+  background-image: url("../../../static/img21.jpg");
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
