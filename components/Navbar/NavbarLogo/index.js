642760e7399388881b58c31aef35777af4b2ba6f
Linas <l.spukas@gmail.com>
2017-05-27 12:18:41 +0300

    Add company's logo by adding it to the navigation bar

---
+++ components/Navbar/NavbarLogo/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const NavbarLogo = styled.div`
+  display: grid;
+  grid-row: 1 / 2;
+  justify-content: center;
+  align-content: center;
+  color: #fff;
+  font-size: 20px;
+  img {
+    width: 50px;
+  }
+`;
+
+export default NavbarLogo;
